4dc9b73c576fb4a361aaa29d16f28a832ab8db98
GPD-Research <gregorydearth@hotmail.com>
2026-06-25 18:30:19 -0400

    Added constraints to constraints.py

--- src/data/constants.py
+++ src/data/constants.py
@@ -1,1 +1,6 @@
-
+# Constants for the BMI framework
+C = 299792458  # Speed of light (m/s)
+H0 = 67.4      # Hubble constant (km/s/Mpc) [cite: 70]
+OMEGA_M0 = 0.315 # Matter density parameter [cite: 70]
+OMEGA_L0 = 0.685 # Dark energy/cosmological constant density [cite: 70]
+RC = 1.0         # Default Critical curvature threshold (to be calibrated) [cite: 104]
